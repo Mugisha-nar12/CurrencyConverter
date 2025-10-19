d9ae9f7beaf1abb76aebf43a945d0c8b198385ef
mugisha-nar12 <81958382+narcisse-web@users.noreply.github.com>
2025-10-19 16:36:17 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-});
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [react()],
+    define: {
+      'process.env': env
+    }
+  });
+}
